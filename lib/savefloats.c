0f6146e731d9685e238b8e1afd8b51069c7368e1
bakkeby <bakkeby@gmail.com>
2021-02-10 07:15:18 +0100

    mark: savefloats integration

--- lib/savefloats.c
+++ lib/savefloats.c
@@ -1,13 +1,11 @@
 void savefloats(const Arg *arg)
 {
-	Client *c = selmon->sel;
-	if (arg && arg->v)
-		c = (Client*)arg->v;
-	if (!c)
-		return;
-	/* save last known float dimensions */
-	c->sfx = c->x;
-	c->sfy = c->y;
-	c->sfw = c->w;
-	c->sfh = c->h;
+	Client *c = CLIENT;
+	for (c = nextmarked(NULL, c); c; c = nextmarked(c->next, NULL)) {
+		/* save last known float dimensions */
+		c->sfx = c->x;
+		c->sfy = c->y;
+		c->sfw = c->w;
+		c->sfh = c->h;
+	}
 }
